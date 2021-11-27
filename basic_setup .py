e1ecdbf60a4b5ee1333b710ab7b3eecfc9574e5b
IperM <75013976+IperM@users.noreply.github.com>
2021-11-27 20:30:54 +0100

    Update basic_setup .py

--- basic_setup .py
+++ basic_setup .py
@@ -86,91 +86,148 @@ srf = NACCESS_atomic(st[0], naccess_binary=args.naccess_bin)
 #print(vars(st[0]['A'][42]['O']))
 #print(vars(st[0]['A'][42]['O'].xtra['vdw']))
 
+resListA = st[0]['A'].get_list()
+resListE = st[0]['E'].get_list()
+
+Interface_ResiduesA = []
+Interface_ResiduesE =[]
+
+for i in resListA:
+    for j in resListE:
+        
+        try:
+            D = i['CA'].get_coord() - j['CA'].get_coord()
+            distance = np.sqrt(np.sum(D*D))   #numpy needed as coordinates are arrays
+        except KeyError: #There's no CA atom
+            continue
+        
+        if distance <= 8:
+
+            if not i.id[1] in Interface_ResiduesA:
+                Interface_ResiduesA.append(i.id[1])
+
+            if not j.id[1] in Interface_ResiduesE:
+                Interface_ResiduesE.append(j.id[1]) 
+
+            
+
+print("in Chain A ", Interface_ResiduesA)
+print("in chain E ", Interface_ResiduesE) 
 
 
-atoms_chainA = []
-atoms_chainE = []
-interface_distan = 6.0
+
+interface_distan = 8
 Total_Eelec = 0
 Total_Vdw = 0
 Total_atoms = 0
 Total_Solvation = 0  
 Solvation_Atom1 = 0
 Solvation_Atom2 = 0
+
+print()
 print()
 print('PAIRS OF ATOMS INSIDE THE INTERFACE AREA OF THE COMPLEX')
 print('----------------------------------------------------------------')
 
-try:
-    for atom1 in st[0]['A'].get_atoms():
-        for atom2 in st[0]['E'].get_atoms():
-            if atom1.serial_number < atom2.serial_number:    	    
-                distances = np.linalg.norm(atom1.get_coord()-atom2.get_coord())
-                if float(distances) <= interface_distan:
-                    Total_atoms += 2 
-                    print('Atom 1 with ID: ',atom1.id[0])
-                    print('Atom 2 with ID: ',atom2.id[0])
-                    print('The distance between this 2 atoms is: ',distances)
-                
-                    chain1 = atom1.get_parent().get_parent()
-                    chain2 = atom2.get_parent().get_parent() 
-                    print('Atom 1 chain: ',chain1.id)
-                    print('Atom 2 chain: ',chain2.id)
-                    ch1 = atom1.xtra['charge']
-                    ch2 = atom2.xtra['charge']
-                    print('Charge of Atom 1: ',ch1)
-                    print('Charge of Atom 2: ',ch2)
-                    print('Electric Energy of this 2 atoms: ',Eelec(distances, ch1, ch2))
-                    sig1 = atom1.xtra['vdw'].sig
-                    sig2 = atom2.xtra['vdw'].sig
-                    eps1 = atom1.xtra['vdw'].eps
-                    eps2 = atom2.xtra['vdw'].eps
-               
-                    print('Van der Waals Energy of this 2 atoms: ',Evdw(distances, sig1, sig2, eps1, eps2))
-                    sigma = atom1.xtra['vdw'].fsrf
-                    if 'EXP_NACCESS' in atom1.xtra:
-                        Asa = atom1.xtra['EXP_NACCESS']
-                    else:
-                        Asa = 0.0
-         
-                    sigma2 = atom2.xtra['vdw'].fsrf
-                    if 'EXP_NACCESS' in atom2.xtra:
-                        Asa2 = atom2.xtra['EXP_NACCESS']
-                    else:
-                        Asa2 = 0.0
-                
-                
-                    Solvation_Atom1 = Esolvation(sigma,Asa)
-                    Solvation_Atom2 = Esolvation(sigma2,Asa2)
-                    print('Solvation Energy Atom1: ',Esolvation(sigma,Asa))
-                    print('Solvation Energy Atom2: ',Esolvation(sigma2,Asa2))
-                    print('Total Solvation Energy of this pair of atoms: ',Solvation_Atom1*Solvation_Atom2)
-                   
-                    print("Pair of atom number: ",Total_atoms)
-                    Total_Eelec += Eelec(distances,ch1,ch2)
-                    Total_Vdw += Evdw(distances, sig1, sig2, eps1, eps2)
-                    Total_Solvation += Solvation_Atom1+Solvation_Atom2
-                   
-                    print('----------------------------------------------------------------')
-    print('Total Electric Energy of the interface: ',Total_Eelec)
-    print('Total Van der Waals Energy: ',Total_Vdw)
-    print('Solvation Energy of the interface: ',Total_Solvation)
 
-except KeyError:
-    for atom1 in st.get_atoms():
-        print('Atom with ID: ',atom1.id[0])
-        chain1 = atom1.get_parent().get_parent()
-        print('Atom chain: ',chain1.id)
-        sigma = atom1.xtra['vdw'].fsrf
-        if 'EXP_NACCESS' in atom1.xtra:
-            Asa = atom1.xtra['EXP_NACCESS']
-        else:
-            Asa = 0.0
+for i in range(len(Interface_ResiduesA)):
+    for j in range(len(Interface_ResiduesE)):
+        for atom1 in st[0]['A'][Interface_ResiduesA[i]].get_atoms():
+            for atom2 in st[0]['E'][Interface_ResiduesE[j]].get_atoms():
+                if atom1.serial_number < atom2.serial_number:
+                    distances = np.linalg.norm(atom1.get_coord()-atom2.get_coord())
+                    if distances <= interface_distan:
+                        Total_atoms += 2 
+                        print('Atom 1 with ID: ',atom1.id[0])
+                        print('Atom 2 with ID: ',atom2.id[0])
+                        print('The distance between this 2 atoms is: ',distances)
+                    
+                        chain1 = atom1.get_parent().get_parent()
+                        chain2 = atom2.get_parent().get_parent() 
+                        print('Atom 1 chain: ',chain1.id)
+                        print('Atom 2 chain: ',chain2.id)
+                        ch1 = atom1.xtra['charge']
+                        ch2 = atom2.xtra['charge']
+                        print('Charge of Atom 1: ',ch1)
+                        print('Charge of Atom 2: ',ch2)
+                        print('Electric Energy of this 2 atoms: ',Eelec(distances, ch1, ch2))
+                        sig1 = atom1.xtra['vdw'].sig
+                        sig2 = atom2.xtra['vdw'].sig
+                        eps1 = atom1.xtra['vdw'].eps
+                        eps2 = atom2.xtra['vdw'].eps
+                    
+                        print('Van der Waals Energy of this 2 atoms: ',Evdw(distances, sig1, sig2, eps1, eps2))
+                        sigma = atom1.xtra['vdw'].fsrf
+                        if 'EXP_NACCESS' in atom1.xtra:
+                            Asa = atom1.xtra['EXP_NACCESS']
+                        else:
+                            Asa = 0.0
+                
+                        sigma2 = atom2.xtra['vdw'].fsrf
+                        if 'EXP_NACCESS' in atom2.xtra:
+                            Asa2 = atom2.xtra['EXP_NACCESS']
+                        else:
+                            Asa2 = 0.0
+                    
+                    
+                        Solvation_Atom1 = Esolvation(sigma,Asa)
+                        Solvation_Atom2 = Esolvation(sigma2,Asa2)
+                        print('Solvation Energy Atom1: ',Esolvation(sigma,Asa))
+                        print('Solvation Energy Atom2: ',Esolvation(sigma2,Asa2))
+                        print('Total Solvation Energy of this pair of atoms: ',Solvation_Atom1*Solvation_Atom2)
+                        
+                        print("Pair of atom number: ",Total_atoms)
+                        Total_Eelec += Eelec(distances,ch1,ch2)
+                        Total_Vdw += Evdw(distances, sig1, sig2, eps1, eps2)
+                        Total_Solvation += Solvation_Atom1+Solvation_Atom2
+                        
+                        print('----------------------------------------------------------------')
 
-        Solvation_Atom = Esolvation(sigma,Asa)
-        print('Total Solvation Energy of this pair of atoms: ',Solvation_Atom)
-    
-        Total_Solvation += Solvation_Atom      
-        print('----------------------------------------------------------------')
-   
-    print('Solvation Energy of chain',chain1.id,': ',Total_Solvation)
+Total_SolvationA = 0
+for chain in st[0].get_chains():
+    print(chain.id)
+    if chain.id == 'A':
+        for a in range(len(Interface_ResiduesA)):
+            for atom1 in st[0]['A'][Interface_ResiduesA[a]].get_atoms():
+                print('Atom with ID: ',atom1.id[0])
+                chain = atom1.get_parent().get_parent()
+                print('Atom chain A')
+                sigma = atom1.xtra['vdw'].fsrf
+                if 'EXP_NACCESS' in atom1.xtra:
+                    Asa = atom1.xtra['EXP_NACCESS']
+                else:
+                    Asa = 0.0
+
+                Solvation_Atom = Esolvation(sigma,Asa)
+                print('Total Solvation Energy of this pair of atoms: ',Solvation_Atom)
+            
+                Total_SolvationA += Solvation_Atom      
+                print('----------------------------------------------------------------')
+        print('Total Solvation of Monomer A: ',Total_Solvation)
+
+    else:
+        Total_SolvationE = 0
+        for n in range(len(Interface_ResiduesE)):
+            for atom1 in st[0]['E'][Interface_ResiduesE[n]].get_atoms():
+                print('Atom with ID: ',atom1.id[0])
+                print('Atom chain E')
+                sigma = atom1.xtra['vdw'].fsrf
+                if 'EXP_NACCESS' in atom1.xtra:
+                    Asa = atom1.xtra['EXP_NACCESS']
+                else:
+                    Asa = 0.0
+
+                Solvation_Atom = Esolvation(sigma,Asa)
+                print('Total Solvation Energy of this pair of atoms: ',Solvation_Atom)
+            
+                Total_SolvationE += Solvation_Atom      
+                print('----------------------------------------------------------------')
+
+            print('Total Solvation of Monomer E: ',Total_SolvationE)
+
+print('Total Electric Energy of the interface: ',Total_Eelec)
+print('Total Van der Waals Energy of the interface: ',Total_Vdw)
+print('Total Solvation Energy of the interface: ',Total_Solvation)
+print('Total Solvation of Monomer A: ',Total_SolvationA)
+
+ 
